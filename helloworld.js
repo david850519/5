c32670027e3f1d35c57da331ebe4ee6466499736
zitim <s911297@gmail.com>
2016-11-14 22:59:07 +0800

    helloworld

---
+++ helloworld.js
@@ -0,0 +1,10 @@
+var ip   = "127.0.0.1";
+var port = 1337;
+var http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}).listen(port, ip);
+
+console.log("Server running at http://" + ip + ":" + port);
