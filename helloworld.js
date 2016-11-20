33403f4aa99a195540a58b99ce203f8af6769255
zitim <s911297@gmail.com>
2016-11-20 14:47:05 +0800

    Update helloworld.js

--- helloworld.js
+++ helloworld.js
@@ -1,11 +1,12 @@
- var ip   = "127.0.0.1";
- var port = 1337;
- var http = require('http');
+//  var ip   = "127.0.0.1";
+//  var port = 1337;
+//  var http = require('http');
 
- http.createServer(function (req, res) {
-   res.writeHead(200, {'Content-Type': 'text/plain'});
-   res.end('Hello World\n');
- }).listen(port, ip);
+//  http.createServer(function (req, res) {
+//    res.writeHead(200, {'Content-Type': 'text/plain'});
+//    res.end('Hello World\n');
+//  }).listen(port, ip);
 
- console.log("Server running at http://" + ip + ":" + port);
+//  console.log("Server running at http://" + ip + ":" + port);
 // console.log('Hello World\n');
+alert('Hello World\n');
