fd3346d0d046ba3fb399765aa4902389a39d65e6
zitim <s911297@gmail.com>
2016-11-20 15:34:05 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
-var express = require('express');
+//heroku-deploy-demo.js
+var express = require("express");
+var logfmt = require("logfmt");
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
+app.use(logfmt.requestLogger());
 
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('pages/index');
+app.get('/', function(req, res) {
+  res.send('Hello World!');
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+var port = Number(process.env.PORT || 5000);
+app.listen(port, function() {
+  console.log("Listening on " + port);
 });
-
