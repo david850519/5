f7fa23d7b66ba5d0f6ef82e2272a28db81168793
zitim <s911297@gmail.com>
2016-11-20 18:51:19 +0800

    Update index.js

--- index.js
+++ index.js
@@ -10,22 +10,10 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-    response.send('Hello World!');
- // response.render('pages/index');
+   // response.send('Hello World!');
+  response.render('pages/index');
 });
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-// var express = require('express');
-// var app = express();
-
-// app.set('port', (process.env.PORT || 5000));
-
-// app.get('/', function (req, res) {
-//     res.send('Hello World!');
-// });
-
-// app.listen(app.get('port'), function () {
-//     console.log('Node app is running on port', app.get('port'));
-// });
