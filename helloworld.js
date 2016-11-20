1c5b30c85a1d1b3f52efc8a8293ea2b2f3b6c7b6
zitim <s911297@gmail.com>
2016-11-20 15:24:38 +0800

    Update helloworld.js

--- helloworld.js
+++ helloworld.js
@@ -1,10 +1,18 @@
-  var ip   = "127.0.0.1";
-  var port = 1337;
-  var http = require('http');
+var express = require('express');
+var app = express();
 
-  http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World\n');
-  }).listen(port, ip);
+app.set('port', (process.env.PORT || 5000));
 
+app.use(express.static(__dirname + '/public'));
 
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('pages/index');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
