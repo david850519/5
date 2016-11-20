94aa75eed8bf13af271026e3bd174e5884e99ab3
zitim <s911297@gmail.com>
2016-11-20 15:42:50 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,7 @@
-//heroku-deploy-demo.js
-var express = require("express");
-var logfmt = require("logfmt");
-var app = express();
+var http = require("http");
 
-app.use(logfmt.requestLogger());
-
-app.get('/', function(req, res) {
-  res.send('Hello World!');
-});
-
-var port = Number(process.env.PORT || 5000);
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+http.createServer(function(request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.write("It's alive!");
+  response.end();
+}).listen(3000);
